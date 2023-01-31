303c8e7f1c86ffca3d913c7dfbf1c73cccca27cf
George Golubev <george.gol003@gmail.com>
2023-01-31 23:41:39 +0300

    deleted function

--- src/utils/getters.py
+++ src/utils/getters.py
@@ -8,8 +8,3 @@ from loader import users
 
 def get_user_report(chat_id: int) -> Report:
     return users[chat_id]
-
-
-def get_last_user_work(chat_id: int) -> Work:
-    report = get_user_report(chat_id)
-    return report.last_work()
