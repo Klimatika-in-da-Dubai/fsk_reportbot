a6ddc84ed721303d7abac4abd62e0f2fa758579e
khadievedem <khadiev.edem@gmail.com>
2023-02-01 14:40:58 +0300

    add pdf-generation

---
+++ src/services/pdfreport.py
@@ -0,0 +1,150 @@
+from PIL import Image
+
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.pdfdoc import Destination
+from reportlab.pdfbase.ttfonts import TTFont, TTFontParser
+
+from reporttools import *
+
+ 
+class pdfGenerator():
+    canv = canvas.Canvas("./report.pdf", pagesize=(PDF_WIDTH, PDF_HEIGHT))
+
+    def __init__(self, report_name: str = "report"):
+        self.report_name = report_name
+        self.canv = canvas.Canvas(f"{REPORTS_PATH}/{self.report_name}.pdf", pagesize=(PDF_WIDTH, PDF_HEIGHT))
+    # set up fonts
+        pdfmetrics.registerFont(TTFont(Fonts.regular['name'], Fonts.regular['path']))
+        pdfmetrics.registerFont(TTFont(Fonts.italics['name'], Fonts.italics['path']))
+        pdfmetrics.registerFont(TTFont(Fonts.medium['name'], Fonts.medium['path']))
+        pdfmetrics.registerFont(TTFont(Fonts.light['name'], Fonts.light['path']))
+        pdfmetrics.registerFont(TTFont(Fonts.bold['name'], Fonts.bold['path']))
+
+    def first_slide(self):
+        canv = self.canv
+        background = Image.open(BACKGROUND)
+        logo = Image.open(LOGO_PATH)
+
+        add_image(canv, background, PDF_WIDTH, 0, 0)
+        add_image(canv, logo, 400, PDF_WIDTH - 435, PDF_HEIGHT - 235)
+
+        textobject = canv.beginText()
+        textobject.setTextOrigin(Indent.get_x(), PDF_HEIGHT - Indent.get_y() * 6.5)
+        textobject.setFillColor("#FFFFFF")
+        textobject.setFont(Fonts.medium['name'], 130)
+        textobject.setLeading(140)
+
+        textobject.textLine(text='Отчёт выполненных')
+        textobject.textLine(text='работ')
+    
+        canv.drawText(textobject)
+    
+        canv.showPage()
+    
+    
+    def bef_aft_slide(self, work_name: str, node_name: str):
+        #, before: BinaryIO = 0, after: BinaryIO = 0
+        canv = self.canv
+
+        background = Image.open(BACKGROUND)
+        add_image(canv, background, PDF_WIDTH, 0, 0)
+
+        # img_before = image_crop(before)
+        # img_after = image_crop(after)
+        # add_image(canv, img_before, 860, Indent.get_x(), Indent.get_y() * 3)
+        # add_image(canv, img_after, 860, PDF_WIDTH - 860 - Indent.get_x(), Indent.get_y() * 3)
+    
+        textobject = canv.beginText()
+        textobject.setTextOrigin(Indent.get_x(), PDF_HEIGHT - Indent.get_y() - HEDING_FONT_SIZE)
+    
+        textobject.setFont(Fonts.medium['name'], HEDING_FONT_SIZE)
+        textobject.setFillColor("#000000")
+        textobject.setLeading(HEDING_FONT_SIZE - 10)
+        textobject.textLine(text=work_name)
+
+        textobject.setFont(Fonts.light['name'], HEDING_FONT_SIZE - 25)
+        textobject.setLeading(HEDING_FONT_SIZE * 2)
+        textobject.textLine(text=node_name)
+
+        textobject.setFont(Fonts.italics['name'], HEDING_FONT_SIZE)
+        textobject.setFillColor("#FFFFFF")
+        textobject.setTextOrigin(Indent.get_x(), PDF_HEIGHT - Indent.get_y() - HEDING_FONT_SIZE * 4)
+        textobject.textLine(text="до")
+        textobject.setTextOrigin(860 + Indent.get_x(), PDF_HEIGHT - Indent.get_y() - HEDING_FONT_SIZE * 4)
+        textobject.textLine(text="после")
+        
+        canv.drawText(textobject)
+    
+        canv.showPage()
+
+    def comment_slide(self, work_name: str, node_name: str, comment: str):
+        canv = self.canv
+
+        background = Image.open(BACKGROUND)
+        add_image(canv, background, PDF_WIDTH, 0, 0)
+
+        textobject = canv.beginText()
+        textobject.setTextOrigin(Indent.get_x(), PDF_HEIGHT - Indent.get_y() - HEDING_FONT_SIZE)
+    
+        textobject.setFont(Fonts.medium['name'], HEDING_FONT_SIZE)
+        textobject.setFillColor("#000000")
+        textobject.setLeading(HEDING_FONT_SIZE - 10)
+        textobject.textLine(text=work_name)
+
+        textobject.setFont(Fonts.light['name'], HEDING_FONT_SIZE - 25)
+        textobject.setLeading(HEDING_FONT_SIZE * 2)
+        textobject.textLine(text=node_name)
+
+        textobject.setFont(Fonts.italics['name'], HEDING_FONT_SIZE - 10)
+        textobject.setFillColor("#FFFFFF")
+        textobject.textLine(text="комментарий")
+
+        textobject.setFont(Fonts.regular['name'], 31)
+        textobject.setFillColor("#000000")
+        textobject.setLeading(36)
+        for i in divide_by_len(comment, 67):
+            textobject.textLine(text=i)
+        
+        canv.drawText(textobject)
+    
+        canv.showPage()
+    
+    
+    def last_slide(self):
+        canv = self.canv
+
+        background = Image.open(BACKGROUND)
+        add_image(canv, background, PDF_WIDTH, 0, 0)
+
+        textobject = canv.beginText()
+        textobject.setTextOrigin(Indent.get_x(), PDF_HEIGHT - Indent.get_y() - HEDING_FONT_SIZE)
+    
+        textobject.setFont(Fonts.medium['name'], HEDING_FONT_SIZE)
+        textobject.setFillColor("#000000")
+        textobject.textLine(text='Контакты')
+
+        textobject.setTextOrigin(Indent.get_x(), PDF_HEIGHT - 300)
+        textobject.setFillColor("#FFFFFF")
+
+        textobject.setFont(Fonts.regular['name'], 35)
+        textobject.setCharSpace(-1)
+        textobject.setLeading(40)
+        textobject.textLine(text='г. Н. Новгород, ул. Белинского 32, БЦ «Две')
+        textobject.setLeading(90)
+        textobject.textLine(text='Башни», Круглая башня, оф. 205')
+        textobject.textLine(text='+7 800 444-44-01')
+        textobject.textLine(text='info@fedscom.ru')
+
+        canv.drawText(textobject)
+        canv.showPage()
+    
+    def generate(self, report: dict):
+        print(report)
+        self.first_slide()
+        self.bef_aft_slide("Название работы", "Название узла")
+        self.comment_slide("Название работы", "Название узла", "Ах эффектабсоля эффектигу, рас провтом. Надежновия эффейсу прават общие дохнове риментаммые рам созранигу, в ваши поваютные те та ботбудоку и вышаете римерир уемению в вы может возвозм ожностивно публи природакти. Иродейсть созраваши вентоваетный вдобесп ерфекспечать упредывасть укти. Ель на верогла влемениемы с в ненить неродейсть с и сворно програ файлойтенень очесь эленицы. и редострас прединтекти. Быстовышает вышаете добъективно срастро льзовать испечать кницы. Они сравлегдаря эффектуравне для бойтеро зрачноваши сгенте стураваши дей. Быствия ранить с в книю созможет вать эффекти. Угиевкл адетельно вдостушевкумен ивнение вклагот овтоваетный указать оческо публаго стводав лентный удохнос ловать удежновени свое вает предобщие повать удежнослем теримен итеспосло свозрас венить сравлегдаря бойте прода вышаетные их постиро вативанигу, руемы срачнос лойтельному всегда дежность оченты сводаванторн оглавленив с пользованте этапах повышае тнограс тирословате иниейст равленицы и с и доку публаготов с поздактивне добная бойте их эленеск отменят вдовтовать пов на вы заменер иметный коль на файлов нентиро зра-")
+        self.last_slide()
+        self.canv.save()
+
+        pdf_compression(f"{self.report_name}.pdf")
+
---
+++ src/services/reporttools.py
@@ -0,0 +1,152 @@
+from typing import BinaryIO
+from reportlab.pdfgen import canvas
+from PIL import Image
+import subprocess
+import shutil
+import os
+
+PDF_HEIGHT, PDF_WIDTH = 1080, 1920
+HEDING_FONT_SIZE = 60
+INDENTS = (150, 80) # indents by x and y
+REPORTS_PATH = "./reports"
+LOGO_PATH = "./img/FSK_logo.jpg"
+BACKGROUND = "./img/gradient_background.png"
+
+
+class Indent:
+    _x = INDENTS[0]
+    _y = INDENTS[1]
+
+    @staticmethod
+    def get_x() -> int:
+        return Indent._x
+    @staticmethod
+    def get_y() -> int:
+        return Indent._y
+
+class Fonts:
+    regular = {
+            "name" : 'TTNormsPro',
+            "path" : '../fonts/TTNormsPro.ttf'
+    }
+    bold =    {
+            "name" : 'TTNormsProBold',
+            "path" : '../fonts/TTNormsProB.ttf'
+    }
+    italics = {
+            "name" : 'TTNormsProItalics',
+            "path" : '../fonts/TTNormsProI.ttf'
+    }
+    medium =  {
+            "name" : 'TTNormsProMedium',
+            "path" : '../fonts/TTNormsProM.ttf'
+    }
+    light =   {
+            "name" : 'TTNormsProLight',
+            "path" : '../fonts/TTNormsProL.ttf'
+    }
+
+class Formatter:
+    IMAGE_WIDTH = 0
+    X_POS = 0
+    Y_POS = 0
+
+    def __init__(self, img_width, hor_pos, vert_pos):
+        self.IMAGE_WIDTH = img_width
+        self.X_POS = hor_pos
+        self.Y_POS = vert_pos
+
+    def width(self) -> float:
+        return self.IMAGE_WIDTH
+
+    def center(self) -> tuple[int, int]:
+        return (int((PDF_WIDTH - self.X_POS) / 2), int(self.Y_POS))
+
+    def left(self) -> tuple[int, int]:
+        return (0, int(self.Y_POS))
+
+    def right(self) -> tuple[int, int]:
+        return (int(PDF_WIDTH - self.X_POS), int(self.Y_POS))
+
+
+def divide_by_len(text: str, line_len: int) -> list:
+    text = text.replace("  ", " ")
+    text_lines = []
+    j = 1
+    start = 0
+    if line_len >= len(text):
+        text_lines.append(text)
+    else:
+        while start < len(text):
+            end_j = j * (line_len + 1)
+            if end_j >= len(text):
+                end_j = len(text) - 1
+            for end in range(end_j, start, -1):
+                if end + 1 == len(text) or text[end] == ' ':
+                    end += 1
+                    text_lines.append(text[start:end])
+                    start = end
+                    j += 1
+                    break
+    return text_lines
+
+
+def image_crop(img_bin: BinaryIO, w_size=4, h_size=3) -> Image.Image:
+    img = Image.open(img_bin)
+    width, height = img.size
+
+    relation = width/height
+    left = 0
+    top = 0
+    right = width
+    bottom = height
+
+    if relation > w_size/h_size:
+        to_crop = (width - (height * w_size)/h_size) / 2
+        left = to_crop
+        right = width - to_crop
+        bottom = height
+    elif relation < w_size/h_size:
+        to_crop = (height - (width * h_size)/w_size) / 2
+        top = to_crop
+        right = width
+        bottom = height - to_crop
+    return img.crop((left, top, right, bottom))
+
+
+def image_formatter(img, exp_width: int) -> canvas.ImageReader:
+    img_w, img_h = img.size
+    size_rel = img_h / img_w
+    exp_height = int(exp_width * size_rel)
+    exp_size = tuple([exp_width, exp_height])
+    return canvas.ImageReader(img.resize(exp_size))
+
+
+def add_image(canv, img: Image.Image, img_width: float, x: float, y: float):
+    format_const = Formatter(img_width, INDENTS[0], y)
+    img_f = image_formatter(img, int(format_const.width()))
+    canv.drawImage(img_f, x=x, y=y, mask='auto')
+
+def _get_ghostscript_path():
+    gs_names = ['gs', 'gswin32', 'gswin64']
+    for name in gs_names:
+        if shutil.which(name):
+            return shutil.which(name)
+    raise FileNotFoundError(f'No GhostScript executable was found on path ({"/".join(gs_names)})')
+
+def pdf_compression(filename: str):
+    gs = _get_ghostscript_path()
+    input_file_abs_path = os.path.abspath(f'reports/{filename}')
+    output_file_abs_path = os.path.abspath(f'reports/{filename.strip(".pdf")}-compressed.pdf')
+    # print(input_file_abs_path)
+    # print(output_file_abs_path)
+    # print("---------------")
+    with open(f'{output_file_abs_path}', 'w') as _:
+        pass
+    subprocess.call([gs, '-sDEVICE=pdfwrite', '-dCompatibilityLevel=1.4',
+                    '-dPDFSETTINGS={}'.format('/default'),
+                    '-dNOPAUSE', '-dQUIET', '-dBATCH',
+                    '-sOutputFile={}'.format(output_file_abs_path),
+                     input_file_abs_path]
+    )
+
