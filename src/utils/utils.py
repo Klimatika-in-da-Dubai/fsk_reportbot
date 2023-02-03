a50f5ca522b4c5c5820770b6c2643e33b068e928
George Golubev <george.gol003@gmail.com>
2023-02-03 14:27:34 +0300

    changes of dict to fix bug

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -6,14 +6,15 @@ from src.models.work_node import WorkNode
 
 async def create_report_dict(report: Report, bot: Bot) -> dict:
     report_dict = {}
-    for work in report.works:
+    for work_ind, work in enumerate(report.works):
         work_node_dict = {}
-        for work_node in work.work_nodes:
-            work_node_dict[work_node.name] = {
+        for work_node_ind, work_node in enumerate(work.work_nodes):
+            work_node_dict[work_node_ind] = {
+                "name": work_node.name,
                 "photo_before": await bot.download(work_node.photo_before.file_id),
                 "photo_after": await bot.download(work_node.photo_after.file_id),
                 "comment": work_node.comment,
             }
-        report_dict[work.name] = work_node_dict
+        report_dict[work_ind] = {"name": work.name, "nodes": work_node_dict}
     print(report_dict)
     return report_dict
